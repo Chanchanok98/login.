ec142b23e73922a59b7db8c638fabb36c3c5e7d0
Chanchanok <71111824+Chanchanok98@users.noreply.github.com>
2021-09-30 14:02:07 +0700

    commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Login Form by www.sunzan-design.com</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
+<h2 style="color:#00A5C2;padding:20px">page2 ^^</h2>
+
+</body>
+</html>
